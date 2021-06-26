22c638d98e52f335ade6e72618c6463f18b0b23e
Solomon <mcbryansolomon@yahoo.com>
2021-06-26 22:20:27 +0000

    recovering from the git fiasco

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import './fonts/Faible-Bold.otf'
+import './fonts/Faible-Medium.otf'
+import './fonts/Faible-Regular.otf'
+
+import './fonts/SpoofTrial-Bold.otf'
+import './fonts/SpoofTrial-Medium.otf'
+import './fonts/SpoofTrial-Regular.otf'
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
